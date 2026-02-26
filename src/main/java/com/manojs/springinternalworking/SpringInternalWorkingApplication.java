d110ecffd870370bfa8c0fcdeee2420fcfbb0906
Manoj <manojcs6317@gmail.com>
2026-02-26 20:45:53 +0530

    feat(project): initialize spring internal working project

---
+++ src/main/java/com/manojs/springinternalworking/SpringInternalWorkingApplication.java
@@ -0,0 +1,13 @@
+package com.manojs.springinternalworking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInternalWorkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInternalWorkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manojs/springinternalworking/SpringInternalWorkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manojs.springinternalworking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInternalWorkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
